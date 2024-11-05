fe97b130e7e11216488f8ef6215fa470c19dfc29
Joseph <jgoyer3@gmail.com>
2024-11-05 07:24:52 -0500

    add names

--- pvgames.lua
+++ pvgames.lua
@@ -26,13 +26,14 @@ function pvgames.animation_spritesheet(offset, length, multiline)
 end
 
 ---@param dir string
+---@param name string
 ---@return table<string, integer>
-function pvgames.extract_portraits(dir)
+function pvgames.extract_portraits(dir, name)
 	local portraitSheet = io.decode(io.path_join(dir, PortraitFile))
-	local frames = spritesheet.to_frames(portraitSheet, "portrait_", pvgames.portrait_spritesheet, true)
+	local frames = spritesheet.to_frames(portraitSheet, name, pvgames.portrait_spritesheet, true)
 
 	local portraits = {}
-	for k, v in pairs(pvgames.portraits) do
+	for k, v in pairs(pvgames.portrait_index) do
 		image.name(frames[v + 1], k)
 		portraits[k] = frames[v + 1]
 	end
@@ -43,30 +44,32 @@ end
 
 ---@param dir string
 ---@param portrait string
+---@param name string
 ---@return integer
-function pvgames.extract_portrait(dir, portrait)
+function pvgames.extract_portrait(dir, portrait, name)
 	local portraitSheet = io.decode(io.path_join(dir, PortraitFile))
 	local frame = spritesheet.to_frames(
 		portraitSheet,
-		portrait,
-		spritesheet.sheet(1, 2000, 2000, 4, spritesheet.offset_index(pvgames.portraits[portrait])),
+		name,
+		spritesheet.sheet(1, 2000, 2000, 4, spritesheet.offset_index(pvgames.portrait_index[portrait])),
 		true
-	)[1]
+	)
 
 	image.remove(portraitSheet)
-	return frame
+	return frame[1]
 end
 
 ---@param dir string
+---@param name string
 ---@return table<string, integer>
-function pvgames.extract_animations(dir)
+function pvgames.extract_animations(dir, name)
 	local animSheet = io.decode(io.path_join(dir, SpritesheetFile))
 
 	local animations = {}
 
-	for k, v in pairs(pvgames.animations) do
+	for k, v in pairs(pvgames.animation_data) do
 		local sheet1, sheet2 = pvgames.animation_spritesheet(v.offset, v.length, true)
-		local anim = spritesheet.extract(animSheet, k, sheet1, sheet2, true)
+		local anim = spritesheet.extract(animSheet, name .. "_" .. k, sheet1, sheet2, true)
 
 		animations[k] = anim
 	end
@@ -77,31 +80,33 @@ end
 
 ---@param dir string
 ---@param animation string
+---@param name string
 ---@return integer
-function pvgames.extract_animation(dir, animation)
+function pvgames.extract_animation(dir, animation, name)
 	local animSheet = io.decode(io.path_join(dir, SpritesheetFile))
 
-	local v = pvgames.animations[animation]
+	local v = pvgames.animation_data[animation]
 
 	local sheet1, sheet2 = pvgames.animation_spritesheet(v.offset, v.length, true)
-	local anim = spritesheet.extract(animSheet, animation, sheet1, sheet2, true)
+	local anim = spritesheet.extract(animSheet, name, sheet1, sheet2, true)
 
 	image.remove(animSheet)
 	return anim
 end
 
 ---@param dir string
+---@param name string
 ---@return {portraits: table<string, integer>, animations: table<string, integer>}
-function pvgames.extract(dir)
+function pvgames.extract(dir, name)
 	local sprites = {
-		portraits = pvgames.extract_portraits(dir),
-		animations = pvgames.extract_animations(dir),
+		portraits = pvgames.extract_portraits(dir, name),
+		animations = pvgames.extract_animations(dir, name),
 	}
 
 	return sprites
 end
 
-pvgames.portraits = {
+pvgames.portrait_index = {
 	neutral1 = 0,
 	neutral2 = 1,
 	happy1 = 2,
@@ -120,6 +125,25 @@ pvgames.portraits = {
 	annoyed2 = 15,
 }
 
+pvgames.portrait = {
+	neutral1 = "neutral1",
+	neutral2 = "neutral2",
+	happy1 = "happy1",
+	happy2 = "happy2",
+	sad1 = "sad1",
+	sad2 = "sad2",
+	angry1 = "angry1",
+	angry2 = "angry2",
+	nervous1 = "nervous1",
+	nervous2 = "nervous2",
+	scared = "scared",
+	injured = "injured",
+	thoughtful1 = "thoughtful1",
+	thoughtful2 = "thoughtful2",
+	annoyed1 = "annoyed1",
+	annoyed2 = "annoyed2",
+}
+
 ---@class animation
 ---@field offset integer
 ---@field length integer
@@ -134,7 +158,84 @@ local function create_animation(offset, length)
 	}
 end
 
-pvgames.animations = {
+pvgames.animation = {
+	walk = "walk",
+	run = "run",
+	idle1 = "idle1",
+	idle2 = "idle2",
+	idle3 = "idle3",
+	idle4 = "idle4",
+	idle_fidget1 = "idle_fidget1",
+	idle_fidget2 = "idle_fidget2",
+	idle_fidget3 = "idle_fidget3",
+	talking1 = "talking1",
+	talking2 = "talking2",
+	interact = "interact",
+	use_item = "use_item",
+	sitting = "sitting",
+	climb = "climb",
+	praying = "praying",
+	jump = "jump",
+	sneaking = "sneaking",
+	crouch = "crouch",
+	casting = "casting",
+	die_forward = "die_forward",
+	die_backward = "die_backward",
+	dead1 = "dead1",
+	dead2 = "dead2",
+	dead3 = "dead3",
+	dead4 = "dead4",
+	evade_roll = "evade_roll",
+	hit1 = "hit1",
+	hit2 = "hit2",
+	critical_idle1 = "critical_idle1",
+	critical_idle2 = "critical_idle2",
+	block = "block",
+	drink = "drink",
+	riding1 = "riding1",
+	riding2 = "riding2",
+	riding3 = "riding3",
+	walk_1h = "walk_1h",
+	idle_1h = "idle_1h",
+	attack1_1h = "attack1_1h",
+	attack2_1h = "attack2_1h",
+	attack3_1h = "attack3_1h",
+	fidget_1h = "fidget_1h",
+	walk_2h = "walk_2h",
+	idle_2h = "idle_2h",
+	run_2h = "run_2h",
+	attack1_2h = "attack1_2h",
+	attack2_2h = "attack2_2h",
+	dual_wield_walk = "dual_wield_walk",
+	dual_wield_idle = "dual_wield_idle",
+	dual_wield_attack1 = "dual_wield_attack1",
+	dual_wield_attack2 = "dual_wield_attack2",
+	dual_wield_fidget = "dual_wield_fidget",
+	bow_walk = "bow_walk",
+	bow_idle = "bow_idle",
+	bow_attack1 = "bow_attack1",
+	bow_fidget = "bow_fidget",
+	unarmed_idle = "unarmed_idle",
+	unarmed_attack1 = "unarmed_attack1",
+	unarmed_attack2 = "unarmed_attack2",
+	unarmed_fidget = "unarmed_fidget",
+	polearm_walk = "polearm_walk",
+	polearm_run = "polearm_run",
+	polearm_idle = "polearm_idle",
+	polearm_attack1 = "polearm_attack1",
+	polearm_attack2 = "polearm_attack2",
+	pistol_walk = "pistol_walk",
+	pistol_idle = "pistol_idle",
+	pistol_attack1 = "pistol_attack1",
+	pistol_attack2 = "pistol_attack2",
+	pistol_fidget = "pistol_fidget",
+	rifle_walk = "rifle_walk",
+	rifle_idle = "rifle_idle",
+	rifle_attack1 = "rifle_attack1",
+	rifle_fidget = "rifle_fidget",
+}
+
+pvgames.animation_data = {
 	walk = create_animation(0, 64),
 	run = create_animation(64, 64),
 	idle1 = create_animation(128, 40),
